61808c2c13c78356f1b108912384b5fc687c1e31
sjc2233 Solomon Chang <sjc2233@columbia.edu>
2022-10-04 18:19:48 +0000

    D

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,14 +4,10 @@ class Movie < ActiveRecord::Base
 	end
 
 	def self.with_ratings(ratings_list)
-	# 	if ratings_list.length() == 0
-	# 		return
-	# 	end
-
-	# 	where("rating = '#{ratings_list[0]}'")
-	# # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
-	# #  movies with those ratings
-	# # if ratings_list is nil, retrieve ALL movies
+		if ratings_list.length() == 0
+			return Movie.all
+		end
+		return Movie.where(rating: ratings_list)
 	end
 
 end
