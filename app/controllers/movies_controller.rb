3e3c80a9c5abfddf2d53e3da06dbc2ed727500e1
sjc2233 Solomon Chang <sjc2233@columbia.edu>
2022-10-05 02:39:18 +0000

    etc

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,6 +18,7 @@ class MoviesController < ApplicationController
 		@movies = Movie.with_ratings(@ratings_to_show)
 
 		if params[:sort] == 'title'
+      @sort = 'title'
 			@title_header = 'hilite p-3 mb-2 bg-warning text-dark'
       @movies = Movie.sortTitle(@ratings_to_show)
 		end
